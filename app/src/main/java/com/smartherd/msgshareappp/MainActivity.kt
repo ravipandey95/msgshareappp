9b3504a8509b81145e6774ab3fadfef7ab3c08e9
rkp91 <rkp9159@gmail.com>
2024-03-13 01:46:02 +0530

    latest

--- app/src/main/java/com/smartherd/msgshareappp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareappp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.smartherd.msgshareappp
 
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickme = findViewById<Button>(R.id.Button)
+
+        clickme.setOnClickListener {
+
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
