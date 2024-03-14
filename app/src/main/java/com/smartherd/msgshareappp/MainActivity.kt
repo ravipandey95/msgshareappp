5ae168ccb42fdf0166400c92a09efed1046b307b
rkp91 <rkp9159@gmail.com>
2024-03-14 18:11:27 +0530

    latest

--- app/src/main/java/com/smartherd/msgshareappp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareappp/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.smartherd.msgshareappp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val clickme = findViewById<Button>(R.id.Button)
-
+        val clickme = findViewById<Button>(R.id.Btnshowtoast)
         clickme.setOnClickListener {
-
             Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
+        val clickmetwo = findViewById<Button>(R.id.btnSendMsgToNextActivity)
+        clickmetwo.setOnClickListener {
+            val usermsg = findViewById<EditText>(R.id.etusermessage)
+            val message : String = usermsg.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/smartherd/msgshareappp/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.smartherd.msgshareappp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_layout)
+    }
+}
