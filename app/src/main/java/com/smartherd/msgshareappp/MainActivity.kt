18d4873a7f2e1413c2b7bb4b656916c9e9cfea02
rkp91 <rkp9159@gmail.com>
2024-03-15 17:42:12 +0530

    sharemsgusingimplicit

--- app/src/main/java/com/smartherd/msgshareappp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareappp/MainActivity.kt
@@ -2,11 +2,11 @@ package com.smartherd.msgshareappp
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
-import android.widget.Toast
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -14,18 +14,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val usermsg = findViewById<EditText>(R.id.etusermessage)
         val clickme = findViewById<Button>(R.id.Btnshowtoast)
         clickme.setOnClickListener {
             Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
         }
         val clickmetwo = findViewById<Button>(R.id.btnSendMsgToNextActivity)
         clickmetwo.setOnClickListener {
-            val usermsg = findViewById<EditText>(R.id.etusermessage)
             val message : String = usermsg.text.toString()
             val intent = Intent(this, SecondActivity::class.java)
 
             intent.putExtra("user_message", message)
             startActivity(intent)
+        }
+
+        val clickthree = findViewById<Button>(R.id.btnShareToOtherApps)
+        clickthree.setOnClickListener{
+            val message : String = usermsg.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, " Share To :"))
         }
     }
 }
